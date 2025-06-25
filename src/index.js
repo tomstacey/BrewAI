399c8d58cbdce6f2b6844f24e7452d7034262406
tomstacey <tomstacey@gmail.com>
2025-06-25 10:58:11 +0100

    fixed paths in js files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Use createRoot from 'react-dom/client' for React 18+
-import './index.css'; // You might need to create this file, or remove if not using
 import App from './App'; // This imports your main App component
 
 // Use createRoot for React 18+ for better performance and new features
